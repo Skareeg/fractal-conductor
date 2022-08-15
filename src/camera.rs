9d79838b034b5d45dfb9139e971355c145ca7aca
Skareeg <skareeg@gmail.com>
2022-08-15 23:14:44 +0000

    Clean up of camera.

--- src/camera.rs
+++ src/camera.rs
@@ -1,16 +1,8 @@
 use bevy::prelude::*;
-use crate::util::{BigTransform, BigPoint, BigVector};
-use nalgebra as na;
 
 #[derive(Component)]
 pub struct Camera {
-    pub transform: BigTransform,
 }
 
 impl Camera {
-    pub fn new(eye: BigPoint, target: BigPoint, up: BigVector) {
-        Self {
-            transform: BigTransform::look_at_rh(eye, target, up)
-        }
-    }
 }
